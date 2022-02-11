0caf5db5053af47f461a663173d0f6827865fd67
Jurgen Hasmeta <jurgenhasmeta12@yahoo.com>
2022-02-11 11:46:59 +0100

    did the querys

--- index.ts
+++ index.ts
@@ -1,14 +1,27 @@
 // #region 'Importing anc Config stuff'
-import express from 'express';
+import express from 'express'
+
 import {
     items, users, services, companies, 
     favorites, subscribe, articles, coaches
 } from "./db/db"
 
+import {
+    Item, Service, Coach, Article, Company, Subscribe, Favorite, User
+} from './types/typesDb'
+
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const url = require('url');
+// const querystring = require('querystring');
+
 const app = express();
 const cors = require('cors')
 const PORT: number = 8000;
 app.use(cors())
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 // #endregion
 
 // #region 'Getting all arrray"
@@ -17,7 +30,7 @@ app.get('/', (req, res) => {
   res.send(`
     <p style="display:grid; place-items: center; font-size: 26px; color: #000; font-weight: 900;">Welcome to AlbVitaFitness, use / and arrays for navigation in the backend server</p>
     
-    <div style = "display: grid; grid-template-rows: repeat(8,1.2fr); grid-gap: 35px; place-items:center; background-color: #191919; height: 100vh;">
+    <div style = "display: grid; grid-template-rows: repeat(10,1fr); grid-gap: 35px; place-items:center; background-color: #191919; height: 100vh; margin: 0 0; paddig: 0 0; box-sizing: border-box;">
         <a style="color: #fff; text-decoration: none; font-size: 22px;" href = "/items">Go to Items</a>
         <a style="color: #fff; text-decoration: none; font-size: 22px;" href = "/services">Go to Services</a>
         <a style="color: #fff; text-decoration: none; font-size: 22px;" href = "/users">Go to Users</a>
@@ -30,40 +43,117 @@ app.get('/', (req, res) => {
         <a style="color: #fff; text-decoration: none; font-size: 22px;">
             If you want to go to individual items just add an number id after / like items/1 gets the first item
         </a>
+        <a style="color: #fff; text-decoration: none; font-size: 22px;">
+            If you want to get random object from the array you can have endpoint random-["name of the array"] and you have use it in front end etc
+        </a>
     </div>`);
 
 });
 
 app.get('/items', (req, res) => {
-  res.send(items);
+
+    const itemId = req.query.id
+    const itemType = req.query.type
+    const itemName = req.query.name
+    
+    const matchId = items.find(item => item.id === Number(itemId))
+    const matchType = items.filter(item => item.type === String(itemType))
+    const matchName = items.find(item => item.name === String(itemName))
+    
+    if (matchId) {
+        res.send(matchId);
+    }
+
+    else if (matchName) {
+        res.send(matchName)
+    }
+
+    else if (matchType.length > 0) {
+        res.send(matchType)
+    }
+  
+    else {
+        res.send(items)
+    }
+  
+});
+
+app.get('/random-items', (req, res) => {
+    const randomItem: Item = items[Math.floor(Math.random() * items.length)]
+    res.send(randomItem);
 });
 
 app.get('/articles', (req, res) => {
+    // const filters:any = req.query;
+    // const filteredArticles: Article[] = articles.filter(article => {
+    //   let isValid:boolean = true;
+    //   for (let key in filters) {
+    //     console.log(key, article[key], filters[key]);
+    //     isValid = isValid && article[key] == filters[key];
+    //   }
+    //   return isValid;
+    // });
+    // res.send(filteredArticles);
     res.send(articles);
+});
+
+app.get('/random-articles', (req, res) => {
+    const randomArticle: Article = articles[Math.floor(Math.random() * articles.length)]
+    res.send(randomArticle);
 });
 
 app.get('/coaches', (req, res) => {
     res.send(coaches);
 });
 
+app.get('/random-coaches', (req, res) => {
+    const randomCoach: Coach = coaches[Math.floor(Math.random() * coaches.length)]
+    res.send(randomCoach);
+});
+
 app.get('/services', (req, res) => {
     res.send(services);
+});
+
+app.get('/random-services', (req, res) => {
+    const randomService: Service = services[Math.floor(Math.random() * services.length)]
+    res.send(randomService);
 });
 
 app.get('/users', (req, res) => {
     res.send(users);
 });
 
+app.get('/random-users', (req, res) => {
+    const randomUser: User = users[Math.floor(Math.random() * users.length)]
+    res.send(randomUser);
+});
+
 app.get('/companies', (req, res) => {
     res.send(companies);
+});
+
+app.get('/random-companies', (req, res) => {
+    const randomCompany: Company = companies[Math.floor(Math.random() * companies.length)]
+    res.send(randomCompany);
 });
 
 app.get('/favorites', (req, res) => {
     res.send(favorites);
 });
 
+app.get('/random-favorites', (req, res) => {
+    const randomFavorite: Favorite = favorites[Math.floor(Math.random() * favorites.length)]
+    res.send(randomFavorite);
+});
+
 app.get('/subscribe', (req, res) => {
     res.send(subscribe);
+});
+
+app.get('/random-subscribe', (req, res) => {
+    const randomSubscribe: Subscribe = subscribe[Math.floor(Math.random() * subscribe.length)]
+    res.send(randomSubscribe);
 });
 // #endregion
 
@@ -193,6 +283,9 @@ app.get('/users/:id', (req, res) => {
 app.listen(PORT, () => {
   console.log(`
   -------------------------------------------------------------------------------------------------------------------
+  
+  -------------------------------------------------------------------------------------------------------------------
+
   Welcome to AlbVitaFitness backend API, here we have all our data and stuff stored, you can acces it for good use!
   
   Server running on: http://localhost:${PORT}
@@ -207,6 +300,9 @@ app.listen(PORT, () => {
   Go to articles : http://localhost:${PORT}/articles
   
   If you want to go to individual items just add an number id after / like items/1 gets the first item
+  If you want to get random object from the array you can have endpoint random-["name of the array"] and you have use it in front end etc
+  -------------------------------------------------------------------------------------------------------------------
+
   -------------------------------------------------------------------------------------------------------------------------`);
 })
 // #endregion
